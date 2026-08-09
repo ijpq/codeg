0cbc498ff057b7cc6f51f1477d65c7929a037683
ijpq <509634578tk@gmail.com>
2026-08-09 07:36:36 +0000

    fix(test): preserve chat authoring in deliverables e2e

--- src-tauri/tests/delegation_e2e_uds.rs
+++ src-tauri/tests/delegation_e2e_uds.rs
@@ -255,6 +255,7 @@ async fn end_to_end_uds_publish_deliverables_replaces_only_the_active_turn() {
         Arc::new(NoSessionInfo) as Arc<dyn codeg_lib::acp::session_info::SessionInfoAccess>,
         Arc::new(NoTaskTools)
             as Arc<dyn codeg_lib::acp::work_task_tools::WorkTaskToolAccess>,
+        Arc::new(NoAuthoring) as Arc<dyn codeg_lib::acp::chat_authoring::ChatAuthoringAccess>,
         Arc::new(DbSessionDeliverableAccess::new(
             db.conn.clone(),
             EventEmitter::Noop,
